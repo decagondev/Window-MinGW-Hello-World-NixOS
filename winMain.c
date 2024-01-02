a02f08938b533857621a6066ce39058fa8a37502
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-02 03:18:42 +0000

    add WndProc()

--- winMain.c
+++ winMain.c
@@ -2,6 +2,12 @@
 #include <windows.h>
 #include <stdio.h>
 
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    return 0;
+}
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
     return 0;
