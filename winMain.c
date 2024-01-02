619495e115c23880c23fe619c6219e159791bcd1
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-02 03:19:50 +0000

    added switch to WndProc with default

--- winMain.c
+++ winMain.c
@@ -5,6 +5,11 @@
 
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
+    switch(msg)
+    {
+        default:
+        return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
     return 0;
 }
 
