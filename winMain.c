e9577e0f71156f88a115287a41dbb82e0baf53c8
Tom Tarpey <tomtarpeydev@gmail.com>
2024-01-02 03:22:10 +0000

    completed WndProc boilerplate

--- winMain.c
+++ winMain.c
@@ -8,9 +8,13 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     switch(msg)
     {
         case WM_CLOSE:
-        DestroyWindow(hwnd);
+            DestroyWindow(hwnd);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
         default:
-        return DefWindowProc(hwnd, msg, wParam, lParam);
+            return DefWindowProc(hwnd, msg, wParam, lParam);
     }
     return 0;
 }
